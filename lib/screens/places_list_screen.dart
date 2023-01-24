a8c0fa3cdadde5b26ab087b28050d85f086ecd4b
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-24 20:59:36 +0100

    Storing & Fetching Data with SQLite

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -20,26 +20,35 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatePlaces>(
-        child: const Center(
-          child: Text('Got no places yet, start adding some'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      greatPlaces.items[i].image,
+      body: FutureBuilder(
+        future: Provider.of<GreatePlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Consumer<GreatePlaces>(
+                    child: const Center(
+                      child: Text('Got no places yet, start adding some'),
                     ),
+                    builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
+                        ? ch
+                        : ListView.builder(
+                            itemCount: greatPlaces.items.length,
+                            itemBuilder: (ctx, i) => ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: FileImage(
+                                  greatPlaces.items[i].image,
+                                ),
+                              ),
+                              title: Text(greatPlaces.items[i].title),
+                              onTap: () {
+                                // Go to detail page ...
+                              },
+                            ),
+                          ),
                   ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    // Go to detail page ...
-                  },
-                ),
-              ),
       ),
     );
   }
