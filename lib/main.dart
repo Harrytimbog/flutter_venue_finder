7a7e60d3c1179cf8eaf9863fbe8b6412804f46f9
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-24 10:01:56 +0100

    Adding the 'Add Place' Screen & An Image Input

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
               .copyWith(secondary: Colors.amber),
         ),
         home: const PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => const AddPlaceScreen(),
+        },
       ),
     );
   }
