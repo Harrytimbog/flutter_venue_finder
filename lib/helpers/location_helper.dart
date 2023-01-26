ed2138b60523e66402decc9f4728e0480d8d345c
Timilehin Arigbede <harriemannie@gmail.com>
2023-01-26 12:50:36 +0100

    Storing the Location in SQLite

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,11 +1,17 @@
-// const GOOGLE_API_KEY =
-//     'pdct.1.1.20230125T104712Z.7ae350bdd85d961c.b79939771ccdc2601cc3dc8031489be222cd3809';
-// const YANDEX_API_KEY =
-//     'pdct.1.1.20230125T104712Z.7ae350bdd85d961c.b79939771ccdc2601cc3dc8031489be222cd3809';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LocationHelper {
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
     return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/$longitude,$latitude,8.29,0/300x200?access_token=pk.eyJ1IjoiaGFycnl0aW1ib2ciLCJhIjoiY2tmbWl1bHhtMDFzcTJzbXBxNHFmaDNmZCJ9.aHKzB2URidomsKAIN97dag';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url = Uri.parse(
+        'https://api.mapbox.com/geocoding/v5/mapbox.places/$lng,$lat.json?types=place%2Cpostcode%2Caddress&limit=1&access_token=pk.eyJ1IjoiaGFycnl0aW1ib2ciLCJhIjoiY2tmbWl1bHhtMDFzcTJzbXBxNHFmaDNmZCJ9.aHKzB2URidomsKAIN97dag');
+    final response = await http.get(url);
+    // print(json.decode(response.body)['features']);
+    return json.decode(response.body)['features'][0]['place_name'];
   }
 }
--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,28 +3,46 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import '../models/place.dart';
 import '../helpers/db_helper.dart';
+import '../helpers/location_helper.dart';
 
-class GreatePlaces with ChangeNotifier {
+class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
   List<Place> get items {
     return [..._items];
   }
 
-  void addPlace(String pickedTitle, File pickedImage) {
+  Future<void> addPlace(
+    String pickedTitle,
+    File pickedImage,
+    PlaceLocation pickedLocation,
+  ) async {
+    final address = await LocationHelper.getPlaceAddress(
+        pickedLocation.latitude, pickedLocation.longitude);
+    final updatedLocation = PlaceLocation(
+      latitude: pickedLocation.latitude,
+      longitude: pickedLocation.longitude,
+      address: address,
+    );
     final newPlace = Place(
       id: DateTime.now().toString(),
       title: pickedTitle,
-      location: null,
+      location: updatedLocation,
       image: pickedImage,
     );
     _items.add(newPlace);
     notifyListeners();
-    DBHelper.insert('user_places', {
-      'id': newPlace.id,
-      'title': newPlace.title,
-      'image': newPlace.image.path
-    });
+    DBHelper.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'loc_lat': newPlace.location.latitude,
+        'loc_lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
+    );
   }
 
   Future<void> fetchAndSetPlaces() async {
@@ -34,7 +52,11 @@ class GreatePlaces with ChangeNotifier {
           (item) => Place(
             id: item['id'],
             title: item['title'],
-            location: null,
+            location: PlaceLocation(
+              latitude: item['loc_data'],
+              longitude: item['lng_data'],
+              address: item['address'],
+            ),
             image: File(item['image']),
           ),
         )
